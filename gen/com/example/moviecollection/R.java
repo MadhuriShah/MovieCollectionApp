808d56dc0d24132fa320c31d9e1d1d8daef5e78e
MadhuriShah <madhurishah393@gmail.com>
2015-03-26 18:18:04 -0400

    ADD/Edit page designed

--- gen/com/example/moviecollection/R.java
+++ gen/com/example/moviecollection/R.java
@@ -25,20 +25,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int detailsScrollView=0x7f080000;
-        public static final int directorLabelTextView=0x7f080005;
-        public static final int directorTextView=0x7f080006;
-        public static final int nameTextView=0x7f080002;
-        public static final int ratingLabelTextView=0x7f080007;
-        public static final int ratingTextView=0x7f080008;
-        public static final int titleLabelTextView=0x7f080001;
-        public static final int yearLabelTextView=0x7f080003;
-        public static final int yearTextView=0x7f080004;
+        public static final int action_settings=0x7f08000f;
+        public static final int addEditScrollView=0x7f080000;
+        public static final int detailsScrollView=0x7f080006;
+        public static final int directorEditText=0x7f080003;
+        public static final int directorLabelTextView=0x7f08000b;
+        public static final int directorTextView=0x7f08000c;
+        public static final int nameTextView=0x7f080008;
+        public static final int ratingEditText=0x7f080004;
+        public static final int ratingLabelTextView=0x7f08000d;
+        public static final int ratingTextView=0x7f08000e;
+        public static final int saveMovieButton=0x7f080005;
+        public static final int titleEditText=0x7f080001;
+        public static final int titleLabelTextView=0x7f080007;
+        public static final int yearEditText=0x7f080002;
+        public static final int yearLabelTextView=0x7f080009;
+        public static final int yearTextView=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int view_details=0x7f030001;
+        public static final int add_edit=0x7f030001;
+        public static final int view_details=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -46,7 +53,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int button_save_movie=0x7f05000b;
         public static final int hello_world=0x7f050001;
+        public static final int hint_director=0x7f050009;
+        public static final int hint_rating=0x7f05000a;
+        public static final int hint_title=0x7f050007;
+        public static final int hint_year=0x7f050008;
         public static final int label_director=0x7f050005;
         public static final int label_rating=0x7f050006;
         public static final int label_title=0x7f050003;
